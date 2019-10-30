de933188809eb34e365ca4029dcd1ba246b9c284
mackg <18511014662@163.com>
2019-10-30 18:12:36 +0800

    加载心教育视频

--- PsychometricNetwork/js/public.js
+++ PsychometricNetwork/js/public.js
@@ -3,3 +3,20 @@ var globalurl="http://192.168.1.145:8886";
 function geturl(){
 	return globalurl;
 }
+
+var v = new Vue({
+	el: "#content",
+	data: {
+		"token":"",
+		"user":"",
+		"isLogin":false
+	},
+	methods:{
+		
+	}
+});
+$(function(){
+	v.token = sessionStorage.getItem("token");
+	v.user = sessionStorage.getItem("user");
+	v.isLogin = sessionStorage.getItem("isLogin");
+})
