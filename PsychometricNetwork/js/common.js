977cb9c0c6af9843ddeb0cb1fd0dd469f907b133
waimsgit <waims123456>
2019-11-06 17:36:00 +0800

    火狐浏览器的兼容

--- PsychometricNetwork/js/common.js
+++ PsychometricNetwork/js/common.js
@@ -41,22 +41,46 @@ window.onload = function(){
 	    }; 
 	}
 	
+		
 		//以下是调用上面的函数
 		var mb = myBrowser();
 		if ("IE" == mb) {
-		    alert("我是 IE");
+//		    alert("我是 IE");
 		}
+		
+		//判断ie版本
+		if (isIE) {
+	        var IE5 = IE55 = IE6 = IE7 = IE8 = false;
+	        var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+	        reIE.test(userAgent);
+	        var fIEVersion = parseFloat(RegExp["$1"]);
+	        IE8 = fIEVersion == 8.0;
+	        var linkNode = document.createElement("link"),
+			scriptNode = document.createElement("script");
+			linkNode.setAttribute("rel", "stylesheet");
+			linkNode.setAttribute("type", "text/css");
+			scriptNode.setAttribute("type", "text/javascript");
+	      
+	        if (IE8) {
+	            return "IE8";
+	            linkNode.setAttribute("href", "css/all-ie-only.css");
+				scriptNode.setAttribute("src", "");
+	        }
+		}//isIE end
+
 		if ("FF" == mb) {
-		    alert("我是 Firefox");
+//		    alert("我是 Firefox");
 		}
 		if ("Chrome" == mb) {
-		    alert("我是 Chrome");
+//		    alert("我是 Chrome");
 		}
 		if ("Opera" == mb) {
-		    alert("我是 Opera");
+//		    alert("我是 Opera");
 		}
 		if ("Safari" == mb) {
-		    alert("我是 Safari");
+//		    alert("我是 Safari");
 		}
+
+
 
 
