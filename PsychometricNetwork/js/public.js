b71bf657d1908cf8429e968dc36a555020ba083e
mackg <18511014662@163.com>
2019-11-07 16:39:04 +0800

    重新封装了ajax请求

--- PsychometricNetwork/js/public.js
+++ PsychometricNetwork/js/public.js
@@ -3,20 +3,56 @@ var globalurl="http://192.168.1.145:8886";
 function geturl(){
 	return globalurl;
 }
+var HttpRequest = function (options) {
+  var defaults = {
+    type: 'post',
+    headers: {},
+    data: {},
+    dataType: 'json',
+    async: true,
+    cache: false,
+    beforeSend: null,
+    success: null,
+    complete: null
+  };
+  var o = $.extend({}, defaults, options);
+  $.ajax({
+    url: globalurl + o.url,
+    type: o.type,
+    headers: {
+      'Content-Type': o.contentType,
+      'token': o.token
+    },
+    data: o.data,
+    dataType: o.dataType,
+    async: o.async,
+    beforeSend: function () {
+      o.beforeSend && o.beforeSend();
+    },
+    success: function (res) {
+      o.success && o.success(res);
+    },
+    complete: function () {
+      o.complete && o.complete();
+    }
+  });
+};
 
-//var v = new Vue({
-//	el: "#content",
-//	data: {
-//		"token":"",
-//		"user":"",
-//		"isLogin":false
-//	},
-//	methods:{
-//		
-//	}
-//});
-//$(function(){
-//	v.token = sessionStorage.getItem("token");
-//	v.user = sessionStorage.getItem("user");
-//	v.isLogin = sessionStorage.getItem("isLogin");
-//})
+var loginHttp = function (options) {
+  // 登入页无需携带token
+  // 后台如果要求 Content-Type 
+  if (options.type == 'post') {
+    options.contentType = 'application/x-www-form-urlencoded';
+  }
+  HttpRequest(options);
+}
+var ajaxHttp = function (options) {
+  if (options.type == 'post') {
+    options.contentType = 'application/x-www-form-urlencoded';
+  }
+  // 每次请求携带token
+  
+  options.token = localStorage.getItem('token');
+  
+  HttpRequest(options);
+}
